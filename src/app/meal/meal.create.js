c514dd6d082ed2e9a185d90759b3d49a7fabe4a8
fsattar <fsattar918@gmail.com>
2015-03-27 11:21:59 -0400

    added delete button for meal dishes

--- src/app/meal/meal.create.js
+++ src/app/meal/meal.create.js
@@ -1,6 +1,6 @@
 angular.module('cms.meal')
 .controller('CreateMealController', function MealController( $scope, $state, notifications, User, Trainer, Delivery, NutritionTag, GooglePlacesFactory, MealFactory, DeliveryFactory) {
-  
+
   var meal = MealFactory.getService();
   var trainers;
   var selectOneSettings = {
@@ -81,7 +81,7 @@ angular.module('cms.meal')
   };
 
   $scope.trainers = [];
-  
+
   $scope.meal = meal.model;
 
   Trainer.find().$promise
@@ -116,8 +116,14 @@ angular.module('cms.meal')
     }
   };
 
-  $scope.addDish = function() {
+  $scope.addDish = function addDish() {
     $scope.meal.dishes.push({});
+  };
+
+  $scope.removeDish = function removeDish(index) {
+    if (index > -1 && $scope.meal.dishes.length > 1) {
+      $scope.meal.dishes.splice(index, 1);
+    }
   };
 
   function setTrainers(trainers) {
